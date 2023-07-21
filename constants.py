29e793d154ec8f21cdca3bde5f4f77dc8498d6d3
Jingjing Liu <jl9358@nyu.edu>
2023-07-21 12:52:06 -0400

    remove key

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 GPT_MODEL = "gpt-3.5-turbo"
 COMPLETION_MODEL = "text-davinci-003"
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "xxxxx"
 
 chatCompletionURL = "https://api.openai.com/v1/chat/completions"
 completionURL = "https://api.openai.com/v1/completions"
